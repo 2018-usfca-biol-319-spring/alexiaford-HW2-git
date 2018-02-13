08a5904ac7274978d4f8df31083059129351d903
Alexia Ford <alexiaford@Aprals-MacBook-Pro-3.local>
2018-02-12 18:14:25 -0800

    Add code for "unde" leaves

--- code/analyze_litterfall.R
+++ code/analyze_litterfall.R
@@ -2,3 +2,25 @@
 # Alexia Ford
 # February 8, 2018
 
+# read in the data
+litter_data <- read.csv("data/ltr_massdata.csv",
+                        header = TRUE)
+
+#subset out data from the University of Notre Dame Environmental Research Center
+unde_data <- subset(litter_data,
+                    siteID == "UNDE")
+
+# let's first look at just leaves mass data
+unde_leaves_data <- subset(unde_data,
+                           functionalGroup == "Leaves")
+
+# make a scatterplot of flower mass by date
+pdf("output/BART_flower_mass_over_time_scatterplot.pdf")   #this u do after plot
+plot(x = as.POSIXct(bart_flower_data$collectDate),
+     y = bart_flower_data$dryMass,
+     main = "Dry mass of flowers fallen in the Bartlett Experimental Forest",
+     xlab = "Date",
+     ylab = "Dry mass of flowers in grams",
+     pch = 16)
+dev.off()
+
