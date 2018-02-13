b0424ffa29057fb80456392a8becf928accf3e59
Alexia Ford <alexiaford@Aprals-MacBook-Pro-3.local>
2018-02-12 18:45:03 -0800

    Add unde and tree code and scatterplot

--- code/analyze_litterfall.R
+++ code/analyze_litterfall.R
@@ -1,6 +1,6 @@
 # A script to visualize NEON data on litterfall
 # Alexia Ford
-# February 8, 2018
+# February 12, 2018
 
 # read in the data
 litter_data <- read.csv("data/ltr_massdata.csv",
@@ -14,13 +14,39 @@ unde_data <- subset(litter_data,
 unde_leaves_data <- subset(unde_data,
                            functionalGroup == "Leaves")
 
-# make a scatterplot of flower mass by date
-pdf("output/BART_flower_mass_over_time_scatterplot.pdf")   #this u do after plot
-plot(x = as.POSIXct(bart_flower_data$collectDate),
-     y = bart_flower_data$dryMass,
-     main = "Dry mass of flowers fallen in the Bartlett Experimental Forest",
+# make a scatterplot of leaves mass by date
+pdf("output/UNDE_leaves_mass_over_time_scatterplot.pdf")   #this u do after plot
+plot(x = as.POSIXct(unde_leaves_data$collectDate),
+     y = unde_leaves_data$dryMass,
+     main = "Dry mass of leaves fallen in the University of Notre Dame Experimental Forest",
      xlab = "Date",
-     ylab = "Dry mass of flowers in grams",
+     ylab = "Dry mass of leaves in grams",
      pch = 16)
 dev.off()
 
+# A script to visualize NEON data on litterfall
+# Alexia Ford
+# February 12, 2018
+
+# read in the data
+litter_data <- read.csv("data/ltr_massdata.csv",
+                        header = TRUE)
+
+#subset out data from the University of Wisconsin Environmental Research Center
+tree_data <- subset(litter_data,
+                    siteID == "TREE")
+
+# let's first look at just leaves mass data
+tree_leaves_data <- subset(tree_data,
+                           functionalGroup == "Leaves")
+
+#git
+# make a scatterplot of leaves mass by date
+pdf("output/TREE_leaves_mass_over_time_scatterplot.pdf")   #this u do after plot
+plot(x = as.POSIXct(tree_leaves_data$collectDate),
+     y = tree_leaves_data$dryMass,
+     main = "Dry mass of leaves fallen in the University of Wisconsin Experimental Forest",
+     xlab = "Date",
+     ylab = "Dry mass of leaves in grams",
+     pch = 16)
+dev.off()
